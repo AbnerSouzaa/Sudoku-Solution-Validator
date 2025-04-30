9c849fc63ced111232b9feb2c4803cceeaa56a0b
Abner Souza <73446845+AbnerSouzaa@users.noreply.github.com>
2025-04-29 22:42:46 -0300

    fix: update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+#include "sudoku_validator.h"
 #include <iostream>
 
 int main() {
-    std::cout << "Sudoku Solution Validator!" << std::endl;
+    std::cout << "=== Sudoku Solution Validator ===" << std::endl;
+
+    std::string filename;
+    std::cout << "Digite o nome do arquivo contendo o Sudoku (ex: sudoku.txt): ";
+    std::cin >> filename;
+
+    std::vector<std::vector<int>> board = readSudokuFromFile(filename);
+
+    bool isValid = validateSudoku(board);
+
+    if (isValid) {
+        std::cout << "A solução do Sudoku é válida!" << std::endl;
+    } else {
+        std::cout << "A solução do Sudoku é inválida!" << std::endl;
+    }
+
     return 0;
 }
